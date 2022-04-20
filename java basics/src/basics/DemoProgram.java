3a57e1fc030fc51b586c0151e770e2444bc3f14c
harsvardhan <harsvardhan@INBAWN166699.ad.harman.com>
2022-04-20 10:40:29 +0530

    control statements

--- java basics/src/basics/DemoProgram.java
+++ java basics/src/basics/DemoProgram.java
@@ -3,8 +3,25 @@ package basics;
 public class DemoProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i=10;
+		/*if(i%2==0)
+			System.out.println("even");
+		else
+			System.out.println("odd");*/
+		/*int m=i%2==0?3919:71220;
+		System.out.println(m);*/
+		String M="K";
+		switch(M)
+		{
+		case "H":
+			System.out.println("Harsha");
+			break;
+		case "V":
+			System.out.println("Vardhan");
+			break;
+		default:
+			System.out.println("not match");
+			
+		}
 	}
-
 }
